3e195b53079b01883836f409def7bdf1d4ddd872
blontum <lontumb700@gmail.com>
2024-09-19 19:54:41 -0400

    labs

---
+++ lab1.py
@@ -0,0 +1,16 @@
+# For Lab 1:
+
+# In this lab i am to use the format() method to create a formatted string. 
+
+# I will define three variables: my first_name (string), my age (integer), and my favorite_color (string).
+#  I will then use  format() method to create a sentence with these variables.
+
+#The general structure would be:
+
+first_name = "Bo"
+age        = 65
+color      = " Red"
+
+output = "Hello, my name is {}. I am {} years old, and my favorite color is {}.".format(first_name, age, color )
+
+print(output)
---
+++ lab2.py
@@ -0,0 +1,15 @@
+# For Lab 2:
+
+# This lab introduces f-strings, a more modern way of formatting strings in Python. 
+
+# I will define two variables: city (string) and temperature (float).
+# I will then go ahead to Use an f-string to create a sentence with these variables.
+
+# The general structure would be:
+
+city        = "Blackwood"
+temperature = 25.7
+
+Result = f"The current temperature in {city} is {temperature}°C"
+
+print(Result)
