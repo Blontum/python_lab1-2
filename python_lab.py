1bd11ae3bcfc7067c95cd6478440f16dae9f5940
blontum <lontumb700@gmail.com>
2024-09-30 20:08:19 -0400

    python lab

---
+++ python_lab.py
@@ -0,0 +1,23 @@
+msg1 = " welcome to aws python 101 class: strings"
+msg2 = " The instructor here is Claudio"
+
+# 1. Capitalize all the first Xters of each word of msg1
+capitalized_msg1 = msg1.title()
+print(capitalized_msg1)
+
+# 2. Reveverse msg2 
+reversed_msg2 = msg2[ : :-1 ]
+print(reversed_msg2)
+
+# 3. write msg1 in lowercase
+lowercase_msg = msg1.lower()
+print(lowercase_msg)
+
+# 4. write msg2 in uppercase
+uppercase_msg2 = msg1.lower()
+print(uppercase_msg2)
+
+# 5. Find how many "e" charaters were used in msg2
+e_count_msg2 = msg2.count('e')
+print(e_count_msg2)
+
